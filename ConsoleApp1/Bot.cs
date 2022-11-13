b176548678a4d2dbac1b7fc9cc3ea3ef098f6cb5
Misha Odozhiy <misha.odozhiy.04@mail.ru>
2022-11-13 17:20:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Bot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Bot
+    {
+        public void ChatBot ()
+        {
+            Console.WriteLine("Привет");
+            string text = Console.ReadLine();
+            if (text.ToLower() == "привет")
+            {
+                Console.WriteLine("Как дела?");
+            }
+
+            text = Console.ReadLine();
+            if (text.ToLower() == "отлично" || text.ToLower() == "нормально" || text.ToLower() == "плохо")
+            {
+                Console.WriteLine("Как вы себя чувствуете?");
+            }
+
+            text = Console.ReadLine();
+            if (text.ToLower() == "отлично" || text.ToLower() == "нормально" || text.ToLower() == "плохо")
+            {
+                Console.WriteLine("Вас не беспокоят ночные кошмары?");
+            }
+
+            text = Console.ReadLine();
+            if (text.ToLower() == "да" || text.ToLower() == "нет")
+            {
+                Console.WriteLine("Вас не посещает чувство что за вами кто то наблюдает?");
+            }
+
+            text = Console.ReadLine();
+            if (text.ToLower() == "да")
+            {
+                Console.WriteLine("Тогда советую вам не смотреть в сторону той двери которая находиться прямо за вашей спиной");
+            }
+            else if (text.ToLower() == "нет")
+            {
+                Console.WriteLine("Посмотрите в дверной проём");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            bot.ChatBot();
+        }
+    }
+}
